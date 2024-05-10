4e58e7ba210400c7d0f3cb122c9e63c6276999a1
exylaci <exy@freemail.hu>
2024-05-10 07:49:28 +0200

    xml_rewrap - read from input

--- xml_rewrap/main.cpp
+++ xml_rewrap/main.cpp
@@ -1,51 +1,52 @@
 #include "main.h"
 #include <iostream>
-#include <fstream>
 #include <algorithm>
+#include <string>
 
 int main(int argc, char* argv[]) {
 	auto sourceFileName = getFileNameFromParameter(argc, argv);
 	auto fi = openSourceFile(sourceFileName);
 	auto outputFileName = setOutputFileName(sourceFileName);
 	auto fo = openOutputFile(outputFileName);
-
+	copyFile(fi, fo);
 	closeFiles(fi, fo);
 	std::cout << "File rewrapped successfully." << std::endl;
 }
 
-//Exit if there was't any command line parameter given in the command line.
+// Returns the first parameter from the command line parameters list as the name 
+// of the source file. Exits if there was't any parameter given in the command line.
 std::string getFileNameFromParameter(int argc, char* argv[]) {
 	if (argc < 2) {
 		std::cout << "Input file parameter is missing!   Exiting..." << std::endl;
-		exit(-1);
+		exit(1);
 	}
 	return argv[1];
 }
 
-//Open source file for reading.
+//Open source file for reading. Returns with its filehandler.
 std::ifstream openSourceFile(std::string& fileName) {
 	std::ifstream fi{ fileName };
 	if (!fi.is_open()) {
 		std::cout << "I cannot open for reading the " << fileName << " source file.   Exiting..." << std::endl << std::endl;
-		exit(-1);
+		exit(2);
 	}
 	std::cout << "Opened " << fileName << " file for reading." << std::endl;
 	return fi;
 }
 
-//Prepare the name of the output file based on the name of the input file
+//Prepares the name of the output file based on the name of the input file
 std::string setOutputFileName(std::string& fileName) {
 	std::string outputFileName{ fileName };
 	outputFileName.replace(outputFileName.find(".xml"), 4, "_out.xml");
 	return outputFileName;
 }
 
-//Open source file for reading.
+//Open output file for writing. Returns with its filehandler.
 std::ofstream openOutputFile(std::string& fileName) {
 	std::ofstream fo{ fileName };
 	if (!fo.is_open()) {
 		std::cout << "I cannot open for writing the " << fileName << " output file.   Exiting..." << std::endl << std::endl;
-		exit(-1);
+		exit(3);
 	}
 	std::cout << "Opened " << fileName << " file for writing." << std::endl;
 	return fo;
@@ -55,4 +56,13 @@ std::ofstream openOutputFile(std::string& fileName) {
 void closeFiles(std::ifstream& fi, std::ofstream& fo) {
 	fi.close();
 	fo.close();
+}
+
+//Copy the contens of source file into output file.
+void copyFile(std::ifstream& fi, std::ofstream& fo) {
+	std::string oneLine;
+	do {
+		std::getline(fi, oneLine);
+		std::cout << oneLine << std::endl;
+	} while (!fi.eof());
 }
--- xml_rewrap/main.h
+++ xml_rewrap/main.h
@@ -6,4 +6,5 @@ std::string getFileNameFromParameter(int argc, char* argv[]);
 std::ifstream openSourceFile(std::string& fileName);
 std::string setOutputFileName(std::string& fileName);
 std::ofstream openOutputFile(std::string& fileName);
+void copyFile(std::ifstream& fi, std::ofstream& fo);
 void closeFiles(std::ifstream& fi, std::ofstream& fo);
